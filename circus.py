bcf3128deb3c26fe7d79bb12689384b74f857396
Michał Piątkowski <mcpiatkowski@gmail.com>
2021-05-14 13:42:32 +0200

    cycles decomposed

--- circus.py
+++ circus.py
@@ -9,40 +9,45 @@ elephants_masses = {}
 cycles = []
 cycle = []
 
+
 for counter in range(num_of_elephants):
     elephants_masses[elephants[counter]] = masses[counter]
 
 print("ELEPHANTS MASSES: ", elephants_masses)
 print('')
-for outerindex in range(num_of_elephants-2):
+for outerindex in range(num_of_elephants):
     print('****** outerindex = {} ******'.format(outerindex))
-    print("ELEPHANTS BEFORE SWAP: ", elephants)
+
     current_elephant = elephants[outerindex]
     position = outerindex
 
+    if outerindex != 0:
+        cycles.append(cycle)
+        cache = cycle
+        cycle = []
+        for appended in cycles:
+            if appended == cache:
+                break
+            for number in cache:
+                if number in appended:
+                    cycles.pop()
+                    break
+
     for innerindex in range(outerindex+1, num_of_elephants):
         print('###### innerindex = {} ######'.format(innerindex))
-        print("CURRENT ELEPHANT: ", current_elephant)
         final_position = final_positions.index(current_elephant)
-        print('FINAL POSITION: ', final_position)
+        cycle.append(current_elephant)
+        print("CYCLE: ", cycle)
         if final_position != outerindex:
-            print("MASA ZAMIENIANEGO",
-                  elephants_masses[elephants[final_position]])
-            print("MASA PIERWSZEGO",
-                  elephants_masses[current_elephant])
             effort += (elephants_masses[elephants[final_position]
                                         ] + elephants_masses[current_elephant])
             current_elephant, elephants[final_position] = elephants[final_position], current_elephant
-
-            print("CURRENT ELEPHANT AFTER SWAP: ", current_elephant)
-            print("ELEPHANTS AFTER SWAP: ", elephants)
         else:
             elephants[final_position] = current_elephant
-            print("CURRENT ELEPHANT AFTER SWAP: ", current_elephant)
-            print("ELEPHANTS AFTER SWAP: ", elephants)
             print(28 * '*')
             print('')
             break
 
 print("ELEPHANTS: ", elephants)
 print("EFFORT NOT OPTIMIZED: ", effort)
+print("CYCLES: ", cycles)
