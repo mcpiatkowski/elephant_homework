24730f527d4e6d5257b2c87fd48e6bf945950ddf
Michał Piątkowski <mcpiatkowski@gmail.com>
2021-05-14 11:46:39 +0200

    elephant masses

--- circus.py
+++ circus.py
@@ -1,7 +1,19 @@
 num_of_elephants = 6
+masses = [2400, 2000, 1200, 2400, 1600, 4000]
 elephants = [1, 4, 5, 3, 6, 2]
 final_positions = [5, 3, 2, 4, 6, 1]
 
+effort = 0
+elephants_masses = {}
+
+cycles = []
+cycle = []
+
+for counter in range(num_of_elephants):
+    elephants_masses[elephants[counter]] = masses[counter]
+
+print("ELEPHANTS MASSES: ", elephants_masses)
+print('')
 for outerindex in range(num_of_elephants-2):
     print('****** outerindex = {} ******'.format(outerindex))
     print("ELEPHANTS BEFORE SWAP: ", elephants)
@@ -14,7 +26,14 @@ for outerindex in range(num_of_elephants-2):
         final_position = final_positions.index(current_elephant)
         print('FINAL POSITION: ', final_position)
         if final_position != outerindex:
+            print("MASA ZAMIENIANEGO",
+                  elephants_masses[elephants[final_position]])
+            print("MASA PIERWSZEGO",
+                  elephants_masses[current_elephant])
+            effort += (elephants_masses[elephants[final_position]
+                                        ] + elephants_masses[current_elephant])
             current_elephant, elephants[final_position] = elephants[final_position], current_elephant
+
             print("CURRENT ELEPHANT AFTER SWAP: ", current_elephant)
             print("ELEPHANTS AFTER SWAP: ", elephants)
         else:
@@ -26,3 +45,4 @@ for outerindex in range(num_of_elephants-2):
             break
 
 print("ELEPHANTS: ", elephants)
+print("EFFORT NOT OPTIMIZED: ", effort)
