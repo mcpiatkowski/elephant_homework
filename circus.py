a4c933fba2cbd58b6262e0b5f9151e0ce37299e0
Michał Piątkowski <mcpiatkowski@gmail.com>
2021-05-15 14:41:57 +0200

    final effort with two methods combined

--- circus.py
+++ circus.py
@@ -13,11 +13,7 @@ cycle = []
 for counter in range(num_of_elephants):
     elephants_masses[counter+1] = masses[counter]
 
-print("ELEPHANTS MASSES: ", elephants_masses)
-print('')
 for outerindex in range(num_of_elephants):
-    print('****** outerindex = {} ******'.format(outerindex))
-
     current_elephant = elephants[outerindex]
     position = outerindex
 
@@ -33,26 +29,37 @@ for outerindex in range(num_of_elephants):
         cycle = []
 
     for innerindex in range(outerindex+1, num_of_elephants):
-        print('###### innerindex = {} ######'.format(innerindex))
         final_position = final_positions.index(current_elephant)
         cycle.append(current_elephant)
-        print("CYCLE: ", cycle)
         if final_position != outerindex:
             effort += (elephants_masses[elephants[final_position]
                                         ] + elephants_masses[current_elephant])
             current_elephant, elephants[final_position] = elephants[final_position], current_elephant
         else:
             elephants[final_position] = current_elephant
-            print(28 * '*')
-            print('')
             break
-
-print("ELEPHANTS: ", elephants)
-print("EFFORT NOT OPTIMIZED: ", effort)
-print("CYCLES: ", cycles)
 
 for cycle in cycles:
     for index, elephant in enumerate(cycle):
         cycle[index] = elephants_masses[elephant]
 
-print("CYCLES WITH MASSES: ", cycles)
+first_method_only = 0
+second_method_only = 0
+min_mass = min(masses)
+final_effort = 0
+
+for cycle in cycles:
+    if len(cycle) != 1:
+        first_method_only += sum(cycle) + (len(cycle)-2) * min(cycle)
+        second_method_only += sum(cycle) + min(cycle) + \
+            (len(cycle)+1) * min_mass
+        first_method = sum(cycle) + (len(cycle)-2) * min(cycle)
+        second_method = sum(cycle) + min(cycle) + (len(cycle)+1) * min_mass
+        final_effort += min(first_method, second_method)
+        print("FIRST_METHOD IN LOOP: ", first_method)
+        print("SECOND_METHOD IN LOOP: ", second_method)
+        print("FINAL EFFORT IN LOOP: ", final_effort)
+
+print("METHOD ONE EFFORT: ", first_method_only)
+print("SECOND METHOD: ", second_method_only)
+print("FINAL EFFORT: ", final_effort)
