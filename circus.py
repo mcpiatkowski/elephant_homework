87eb151fc3bed2753a0511fe82b7379b85ce1e39
Michał Piątkowski <mcpiatkowski@gmail.com>
2021-05-15 17:46:41 +0200

    one output only

--- circus.py
+++ circus.py
@@ -27,7 +27,6 @@ for counter in range(num_of_elephants):
     elephants_masses[counter+1] = masses[counter]
 
 for outerindex in range(num_of_elephants):
-    print("OUTERINDEX", outerindex)
     current_elephant = elephants[outerindex]
     position = outerindex
 
@@ -43,7 +42,6 @@ for outerindex in range(num_of_elephants):
         cycle = []
 
     for innerindex in range(outerindex+1, num_of_elephants):
-        print("INNERINDEX: ", innerindex)
         final_position = final_positions.index(current_elephant)
         cycle.append(current_elephant)
         if final_position != outerindex:
@@ -58,23 +56,13 @@ for cycle in cycles:
     for index, elephant in enumerate(cycle):
         cycle[index] = elephants_masses[elephant]
 
-first_method_only = 0
-second_method_only = 0
 min_mass = min(masses)
 final_effort = 0
 
 for cycle in cycles:
     if len(cycle) != 1:
-        first_method_only += sum(cycle) + (len(cycle)-2) * min(cycle)
-        second_method_only += sum(cycle) + min(cycle) + \
-            (len(cycle)+1) * min_mass
         first_method = sum(cycle) + (len(cycle)-2) * min(cycle)
         second_method = sum(cycle) + min(cycle) + (len(cycle)+1) * min_mass
         final_effort += min(first_method, second_method)
-        print("FIRST_METHOD IN LOOP: ", first_method)
-        print("SECOND_METHOD IN LOOP: ", second_method)
-        print("FINAL EFFORT IN LOOP: ", final_effort)
 
-print("METHOD ONE EFFORT: ", first_method_only)
-print("SECOND METHOD: ", second_method_only)
-print("FINAL EFFORT: ", final_effort)
+print(final_effort)
